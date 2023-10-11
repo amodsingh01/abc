6a0108a1d2f30c466a0ae7d573b524aec5f5ad48
amodsingh01 <54638981+amodsingh01@users.noreply.github.com>
2023-10-11 18:04:36 +0530

    Composed by AI: variables.tf

--- variables.tf
+++ variables.tf
@@ -24,3 +24,34 @@ variable "machine_type" {
 variable "subnetwork" {
   description = "The subnetwork to create the GKE cluster in."
 }
+variable "project_id" {
+  description = "The ID of the Google Cloud project"
+}
+
+variable "region" {
+  description = "The region to create the GKE cluster in"
+  default     = "us-central1"
+}
+
+variable "cluster_name" {
+  description = "The name of the GKE cluster"
+}
+
+variable "node_count" {
+  description = "The number of nodes in the GKE cluster"
+  default     = 3
+}
+
+variable "machine_type" {
+  description = "The machine type for the GKE nodes"
+  default     = "n1-standard-1"
+}
+
+variable "subnetwork" {
+  description = "The subnetwork to create the GKE cluster in"
+}
+
+variable "zone" {
+  description = "The zone to create the GKE cluster in"
+  default     = "us-central1-a"
+}
