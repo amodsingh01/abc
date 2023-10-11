97901037ef6bb9c72d16d4701f681c48e9d8472a
amodsingh01 <54638981+amodsingh01@users.noreply.github.com>
2023-10-11 18:04:37 +0530

    Composed by AI: modules/gke_cluster/main.tf

--- modules/gke_cluster/main.tf
+++ modules/gke_cluster/main.tf
@@ -38,3 +38,42 @@ output "endpoint" {
 output "ca_certificate" {
   value = module.gke_cluster.ca_certificate
 }
+module "gke_cluster" {
+  source = "terraform-google-modules/kubernetes-engine/google"
+
+  project_id = var.project_id
+  name       = var.cluster_name
+  region     = var.region
+  subnetwork = var.subnetwork
+  zone       = var.zone
+
+  node_pools = [
+    {
+      name            = "default-pool"
+      machine_type    = var.machine_type
+      initial_node_count = var.node_count
+    }
+  ]
+
+  network_policy = [
+    {
+      enabled = true
+    }
+  ]
+
+  master_authorized_networks_config = [
+    {
+      cidr_blocks = [
+        "0.0.0.0/0"
+      ]
+    }
+  ]
+}
+
+output "cluster_endpoint" {
+  value = module.gke_cluster.endpoint
+}
+
+output "cluster_ca_certificate" {
+  value = module.gke_cluster.master_auth[].cluster_ca_certificate
+}
