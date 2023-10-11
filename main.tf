8925754f57b61c94e564126ca41a6cd29295842d
amodsingh01 <54638981+amodsingh01@users.noreply.github.com>
2023-10-11 18:04:36 +0530

    Composed by AI: main.tf

--- main.tf
+++ main.tf
@@ -19,3 +19,25 @@ output "cluster_endpoint" {
 output "cluster_ca_certificate" {
   value = module.gke_cluster.ca_certificate
 }
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+module "gke_cluster" {
+  source = "./modules/gke_cluster"
+
+  cluster_name = var.cluster_name
+  node_count   = var.node_count
+  machine_type = var.machine_type
+  subnetwork   = var.subnetwork
+  zone         = var.zone
+}
+
+output "cluster_endpoint" {
+  value = module.gke_cluster.cluster_endpoint
+}
+
+output "cluster_ca_certificate" {
+  value = module.gke_cluster.cluster_ca_certificate
+}
